b70e45a22a9b9e2873503a8ebf50a5f62d874c42
Collins <mutaic84@gmail.com>
2022-06-12 10:19:56 +0300

    tic tac toe code lab

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,4 +15,50 @@ class TicTacToe
     [0,4,8],
     [2,4,6]
   ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(board_index, token="X")
+    @board[board_index] = token
+  end
+
+  def position_taken?(prospective_index)
+    @board[prospective_index] != " "
+  end
+
+  def valid_move?(index_position)
+    !position_taken?(index_position) && (0..8).include?(index_position)
+  end
+
+  def turn_count
+    # binding.pry
+    @board.count {|move| move != " "}
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn
+    puts "Enter your move. Should be within range (1-9):"
+    user_input = gets.chomp()
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      player_token = current_player
+      move(index, player_token)
+      display_board
+    else
+      turn
+    end
+  end
 end
